88b3c22fb677949fc2fae57747d6e3e84610b8d1
Giorgi Antia <georgeeantia@gmail.com>
2024-04-02 20:01:04 +0400

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    i18n: {
+        locales: ['ka', 'en'],
+        defaultLocale: 'ka',
+        localeDetection: false,
+    },
+};
 
 export default nextConfig;
