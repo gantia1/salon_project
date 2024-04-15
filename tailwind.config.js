8a8e6d43bd787a25f728779c46c019bf6727d252
Giorgi <giorgi.antia@softgen.ge>
2024-04-15 15:44:37 +0400

    Refactor Tailwind CSS configuration and Next.js setup
    - Refactor Tailwind CSS configuration to add custom colors, background images, fonts, and box shadows.
    - Update Next.js configuration to include transition for Ant Design packages and webpack rule for SVG files.
    - Add dependencies in package.json and update .gitignore to exclude IDE-related files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'primary-black': '#353535',
+        'primary-purple': '#7E41FF',
+        'dark-purple': '#B6A2E1',
+        'light-purple': '#FAF8FF',
       },
+      backgroundImage: {
+        'banner': "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/assets/images/png/rectangle2558.png')",
+      },
+      fontFamily: {
+        customPrimary: ['IngiriArial', 'sans-serif'],
+        customBold: ['NinoMtavruli_Regular', 'sans-serif'],
+      },
+      boxShadow: {
+        custom: '0px 10px 20px 0px #0000001A',
+      },
+
     },
   },
   plugins: [],
