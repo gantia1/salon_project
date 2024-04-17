f81bc52f6e17aaa71e2fc74b22a3c7ae8982ef08
Giorgi <giorgi.antia@softgen.ge>
2024-04-17 11:48:44 +0400

    feat: Add slide border bottom animation to menu items in Header component

--- components/customer/wrapperComps/header/Header.jsx
+++ components/customer/wrapperComps/header/Header.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Link from 'next/link';
 import {usePathname} from 'next/navigation';
 import {useTranslation} from 'next-i18next';
 import PrimaryButton from "@/components/button/PrimaryButton";
 import LanguageSelector from "@/components/languageSelector/LanguageSelector";
+import {ModalsContext} from "@/components/contexts/ModalsContexts";
+import {CustomerContext} from "@/components/contexts/CustomerContext";
+import ProfileSVG from "@/public/assets/images/svg/profile.svg";
 
 function Header() {
     const {t} = useTranslation();
+    const {setShowAuthorizationModal, setShowRegisterModal} = useContext(ModalsContext);
+    const {user} = useContext(CustomerContext);
     const currentPath = usePathname();
 
     const menuItems = [
@@ -17,7 +22,7 @@ function Header() {
         {path: '/salon-registration', label: t('salonRegistration')}
     ];
 
-    const isActive = (path) => currentPath === path ? 'text-primary-purple border-b border-primary-purple pb-5' : 'text-primary-black';
+    const isActive = (path) => currentPath === path;
 
     return (
         <header className="w-full">
@@ -26,15 +31,31 @@ function Header() {
                 <Link href="/">LOGO</Link>
                 <ul className="flex gap-x-3.5 xl:gap-x-[30px]">
                     {menuItems.map((item, index) => (
-                        <li key={index}>
-                            <Link href={item.path} className={isActive(item.path)}>{item.label}</Link>
+                        <li key={index}
+                            className={`${index === menuItems.length - 1 ? 'border-l border-dark-purple pl-3.5 xl:pl-[30px]' : ''} ${isActive(item.path) ? 'slide-border active' : 'slide-border'}`}>
+                            <Link href={item.path} className="block">
+                                {item.label}
+                            </Link>
                         </li>
                     ))}
                 </ul>
+
                 <div className="flex gap-5 items-center">
-                    <button className="text-sm font-customBold font-bold text-primary"
-                            type="button">{t('login')}</button>
-                    <PrimaryButton type="button" onClick={() => console.log('test')} children={t('register')}/>
+                    {user ? (
+                        <>
+                            <ProfileSVG/>
+                            <p>{user.username}</p>
+                        </>
+                    ) : (
+                        <>
+                            <button className="text-sm font-customBold font-bold text-primary" type="button"
+                                    onClick={() => setShowAuthorizationModal(true)}>
+                                {t('login')}
+                            </button>
+                            <PrimaryButton type="button" onClick={() => setShowRegisterModal(true)}
+                                           children={t('register')}/>
+                        </>
+                    )}
                     <LanguageSelector/>
                 </div>
             </nav>
