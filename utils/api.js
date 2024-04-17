5e24ce26d72664983c8f908dc04f20fa54918eec
Giorgi <giorgi.antia@softgen.ge>
2024-04-17 11:41:38 +0400

    refactor: Update axios configuration in api.js

--- utils/api.js
+++ utils/api.js
@@ -1,11 +1,19 @@
-import axios from  'axios';
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.SERVICE,
-    headers: {
-        'Content-Type': "application/json",
-        Accept: 'application/json'
-    },
+    baseURL: 'http://localhost:8000/api/v1/',
+});
+
+api.interceptors.request.use(function (config) {
+    config.headers = config.headers || {};
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    config.params = {...config.params, lang: localStorage.getItem('language')};
+    return config;
+}, function (error) {
+    return Promise.reject(error);
 });
 
 export default api;
