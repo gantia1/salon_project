ff4f46e395e6fcc5255f776df97263761d02122b
Giorgi <giorgi.antia@softgen.ge>
2024-04-19 10:51:13 +0400

    feat: Add .env.example template and use in api.js

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/',
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 api.interceptors.request.use(function (config) {
