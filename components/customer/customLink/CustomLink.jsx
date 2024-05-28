a72f6b69cd1764b77089ad427afc7245a3ca98f7
გიორგი ანთია <giorgi.antia@softgen.ge>
2024-05-28 15:49:23 +0400

    feat: Enhance SalonCard with image display, update CustomLink styles and global.scss

--- components/customer/customLink/CustomLink.jsx
+++ components/customer/customLink/CustomLink.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const CustomLinkWithArrow = ({ href, children }) => {
     return (
-        <div className="flex justify-end">
-            <Link href={href} className="font-customBold font-bold text-base items-center flex justify-end w-fit gap-x-8 my-5">
+        <div className="flex justify-center lg:justify-end">
+            <Link href={href} className="font-customBold font-bold text-base items-center flex justify-end w-fit gap-x-1 lg:gap-x-8 my-5">
                 {children} <Arrow />
             </Link>
         </div>
