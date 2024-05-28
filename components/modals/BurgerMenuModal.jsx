3ba1180908004a1dce5edfeec6cb964439d19d07
გიორგი ანთია <giorgi.antia@softgen.ge>
2024-05-28 13:35:12 +0400

    feat: Add language change functionality to BurgerMenuModal

--- components/modals/BurgerMenuModal.jsx
+++ components/modals/BurgerMenuModal.jsx
@@ -1,19 +1,22 @@
-import React, { useContext, useEffect } from 'react';
-import { Drawer } from "antd";
-import { ModalsContext } from "@/components/contexts/ModalsContexts";
+import React, {useContext, useEffect} from 'react';
+import {Drawer} from "antd";
+import {ModalsContext} from "@/components/contexts/ModalsContexts";
 import Link from "next/link";
-import { useTranslation } from "next-i18next";
-import { useRouter } from 'next/router';
+import GeSVG from '@/public/assets/images/svg/ge.svg';
+import EnSVG from '@/public/assets/images/svg/en.svg';
+import {useTranslation} from "next-i18next";
+import {useRouter} from 'next/router';
 
 function BurgerMenuModal() {
-    const { showBurgerModal, setShowBurgerModal } = useContext(ModalsContext);
-    const { t } = useTranslation();
+    const {showBurgerModal, setShowBurgerModal} = useContext(ModalsContext);
+    const {t} = useTranslation();
     const router = useRouter();
+    const {i18n} = useTranslation();
     const menuItems = [
-        { path: '/', label: t('home') },
-        { path: '/salons', label: t('salons') },
-        { path: '/about-us', label: t('about') },
-        { path: '/contact', label: t('contact') },
+        {path: '/', label: t('home')},
+        {path: '/salons', label: t('salons')},
+        {path: '/about-us', label: t('about')},
+        {path: '/contact', label: t('contact')},
     ];
 
     useEffect(() => {
@@ -28,9 +31,15 @@ function BurgerMenuModal() {
         };
     }, [router.events, setShowBurgerModal]);
 
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        setShowBurgerModal(false);
+    };
+
     return (
         <Drawer title={false} onClose={() => setShowBurgerModal(false)} open={showBurgerModal} size={"430px"}>
-            <ul className="flex flex-col items-center text-[30px] text-white text-center w-fit m-auto">
+            <ul className="flex flex-col items-center text-3xl text-white text-center w-fit m-auto">
                 {menuItems.map((item, index) => (
                     <li key={index} className="border-b border-secondary-gray py-[50px] w-[200px]">
                         <Link href={item.path} className="block">
@@ -38,6 +47,26 @@ function BurgerMenuModal() {
                         </Link>
                     </li>
                 ))}
+            </ul>
+            <ul className="flex flex-col items-center text-xl text-white gap-y-5 w-fit m-auto pt-12">
+                <li>
+                    <button
+                        className={`flex items-center gap-x-5 rounded px-5 py-2.5 lng-btn w-[180px] ${i18n.language === 'ka' ? 'active' : ''}`}
+                        type="button"
+                        onClick={() => changeLanguage('ka')}
+                    >
+                        <GeSVG/> ქართული
+                    </button>
+                </li>
+                <li>
+                    <button
+                        className={`flex items-center gap-x-5 rounded px-5 py-2.5 lng-btn w-[180px] ${i18n.language === 'en' ? 'active' : ''}`}
+                        type="button"
+                        onClick={() => changeLanguage('en')}
+                    >
+                        <EnSVG/> English
+                    </button>
+                </li>
             </ul>
         </Drawer>
     );
