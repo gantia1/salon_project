dfbb0cab27714b90f90157313518a45534bb564f
გიორგი ანთია <giorgi.antia@softgen.ge>
2024-06-05 17:48:19 +0400

    feat: configured Ant Design theme in App component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,19 @@
 import {ModalsContextProvider} from "@/components/contexts/ModalsContexts";
 import CustomerContextProvider from "@/components/contexts/CustomerContext";
+import GuestLayout from "@/components/layouts/GuestLayout";
 import CustomerLayout from "@/components/layouts/CustomerLayout";
 import {appWithTranslation} from "next-i18next";
 import {useEffect, useState} from "react";
 import i18n from "@/i18n";
 import '@/styles/fonts.scss';
 import "@/styles/global.scss";
+import {ConfigProvider} from 'antd';
+
 
 function App({Component, pageProps}) {
     const [isClient, setIsClient] = useState(false);
     const layouts = {
+        GuestLayout: GuestLayout,
         CustomerLayout: CustomerLayout,
         NoLayout: ({children}) => <>{children}</>
     };
@@ -27,9 +31,19 @@ function App({Component, pageProps}) {
     return (
         <CustomerContextProvider>
             <ModalsContextProvider>
+                <ConfigProvider
+                    theme={{
+                        token: {
+                            colorPrimary: '#7E41FF',
+                            colorBorderSecondary: '#7E41FF'
+                        },
+                    }}
+                >
+
                 {isClient && <Layout>
                     <Component {...pageProps} />
                 </Layout>}
+                </ConfigProvider>
             </ModalsContextProvider>
         </CustomerContextProvider>
     );
