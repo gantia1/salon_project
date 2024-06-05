bf36bb0ef1f8eedefa8b7931442769e158963476
გიორგი ანთია <giorgi.antia@softgen.ge>
2024-06-05 17:47:04 +0400

    feat: Added translated title for Procedures component in Home

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,12 +4,14 @@ import Procedures from "@/components/customer/procedures/Procedures";
 import Salons from "@/components/customer/salons/Salons";
 import BannerLower from "@/components/customer/banner/BannerLower";
 import Feedback from "@/components/customer/feedback/Feedback";
+import {useTranslation} from "next-i18next";
 
 export default function Home() {
+    const {t} = useTranslation();
     return (
         <>
             <Banner/>
-            <Procedures/>
+            <Procedures title={t('procedures')}/>
             <Salons/>
             <BannerLower/>
             <Feedback/>
@@ -17,4 +19,4 @@ export default function Home() {
     );
 }
 
-Home.layout = 'CustomerLayout';
+Home.layout = 'GuestLayout';
